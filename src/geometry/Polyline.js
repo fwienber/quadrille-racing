6f5843228432c2904e8b9946a25b736d01263155
mbuse <matthias.buse@coremedia.com>
2019-08-22 16:35:42 +0200

    Polyline added

---
+++ src/geometry/Polyline.js
@@ -0,0 +1,30 @@
+import {Position} from "./Position.js";
+import {Line} from "./Line.js"
+
+export class Polyline {
+
+  constructor(...positions) {
+    this.positions = positions;
+  }
+
+  segmentCount() {
+    return this.positions.length - 1;
+  }
+
+  getSegment(index) {
+    if (index > this.segmentCount()-1 || index<0) {
+      return null;
+    }
+    return new Line(this.positions[index], this.positions[index+1]);
+  }
+
+  intersect(line) {
+    for (let i=0; i< this.segmentCount(); i++) {
+      let intersect = this.getSegment(i).intersect(line);
+      if (intersect) {
+        return intersect;
+      }
+    }
+    return null;
+  }
+}
