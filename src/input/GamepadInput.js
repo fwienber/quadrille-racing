d9f15572ea8d38e7a514151fa7a02b35441a6828
fwienber <frank.wienberg@coremedia.com>
2019-08-23 12:49:06 +0200

    Remove "stop game" gamepad button

--- src/input/GamepadInput.js
+++ src/input/GamepadInput.js
@@ -4,7 +4,6 @@ const CROSS_UP = 12;
 const CROSS_DOWN = 13;
 const CROSS_LEFT = 14;
 const CROSS_RIGHT = 15;
-const BURGER = 9;
 const AXES_LIMIT = 0.5;
 
 export function waitForGamepads(number, callback) {
@@ -33,9 +32,6 @@ class GamepadInput {
     let gamepad = window.navigator.getGamepads()[this.index];
     if (gamepad) {
       let buttons = gamepad.buttons;
-      if (buttons[BURGER].pressed) {
-        return null;
-      }
       let leftRight = gamepad.axes[0];
       let upDown = gamepad.axes[1];
       let deltaX = buttons[CROSS_LEFT].pressed || leftRight < -AXES_LIMIT ? -1
